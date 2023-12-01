fa09a50718d1de083c6856038593da8d846c80ce
sulthanayesha <152604756+sulthanayesha@users.noreply.github.com>
2023-12-01 16:49:10 +0530

    28-11-23 in html css and react

---
+++ array.jsx
@@ -0,0 +1,55 @@
+import './array.css';
+export function Arr() {
+    return (
+        <div>
+            <h1>OBJECT TYPE</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Customer id</th>
+                        <th>f_name</th>
+                        <th>i_name</th>
+                        <th>account_no</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>401</td>
+                        <td>402</td>
+                        <td>403</td>
+                        <td>404</td>
+                    </tr>
+                    <tr>
+                        <td>Menon</td>
+                        <td>Aaron</td>
+                        <td>Kevin</td>
+                        <td>Ben</td>
+                    </tr>
+                    <tr>
+                        <td>Caren</td>
+                        <td>Paul</td>
+                        <td>Peterson</td>
+                        <td>Stock</td>
+                    </tr>
+                    <tr>
+                        <td>2345555</td>
+                        <td>3244555</td>
+                        <td>4235555</td>
+                        <td>5234555</td>
+                    </tr>
+                </tbody>
+
+
+
+
+
+            </table>
+
+        </div>
+
+
+
+
+
+    )
+}
